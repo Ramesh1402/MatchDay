b3c1ede7d7f690ccf1161013b7e4b8c72c3abad3
Ramesh1402 <reachrameshmobiledev@gmail.com>
2025-02-22 15:55:01 +0530

    feat: design matches screen

--- MatchDay/CricketAPIService.swift
+++ MatchDay/CricketAPIService.swift
@@ -43,7 +43,47 @@ struct MatchesResponse: Decodable {
     let data: [Match]
 }
 
-struct Match: Decodable, Identifiable {
+struct Match: Decodable, Identifiable, Hashable {
     let id: String
     let name: String
+    let matchType: String
+    let status: String
+    let teams: [Team]
+}
+
+enum Team: String, Decodable, CaseIterable {
+    case oman
+    case usa
+    case unknown
+    
+    var flagURL: URL? {
+        var countryName: String
+        
+        switch self {
+        case .oman:
+            countryName = "oman"
+        case .usa:
+            countryName = "usa"
+        case .unknown:
+            return nil
+        }
+        
+        return URL(string: "https://www.countryflags.com/wp-content/uploads/\(countryName)-flag-png-large.png")
+    }
+    enum CodingKeys: String, CodingKey {
+        case oman = "Oman"
+        case usa = "United States of America"
+        case unknowm
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let decodedValue = try container.decode(String.self)
+        
+        if let result = Team.allCases.first(where: { $0.rawValue.lowercased() == decodedValue.lowercased() }) {
+            self = result
+        } else {
+            self = .unknown
+        }
+    }
 }
--- MatchDay/MatchesView.swift
+++ MatchDay/MatchesView.swift
@@ -11,26 +11,77 @@ struct MatchesView: View {
     @State var viewModel = MatchesViewModel()
     
     var body: some View {
-        Group {
-            switch viewModel.loadingState {
-            case .loading:
-                ProgressView()
-            case .success(let matches):
-                List {
-                    ForEach(matches) { match in
-                        Text(match.name)
+        NavigationStack {
+            Group {
+                switch viewModel.loadingState {
+                case .loading:
+                    ProgressView()
+                case .success(let matches):
+                    List {
+                        ForEach(matches) { match in
+                            NavigationLink(value: match) {
+                                MatchCardView(match: match)
+                                    .listRowInsets(EdgeInsets())
+                                    .padding(.bottom, 4)
+                                    .background(Color.gray.opacity(0.3))
+                            }
+                        }
                     }
+                case .failure:
+                    Text("Error loading matches")
                 }
-            case .failure:
-                Text("Error loading matches")
             }
-        }
-        .task {
-            await viewModel.loadMatches()
+            .navigationTitle("Current Matches")
+            .navigationDestination(for: Match.self, destination: { match in
+                MatchCardView(match: match)
+                    .listRowInsets(EdgeInsets())
+                    .padding(.bottom, 4)
+                    .background(Color.gray.opacity(0.3))
+            })
+            .task {
+                await viewModel.loadMatches()
+            }
         }
     }
 }
 
-#Preview {
-    MatchesView()
+struct MatchCardView: View {
+    let match: Match
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            HStack {
+                AsyncImage(url: match.teams[0].flagURL) { image in
+                    image
+                        .resizable()
+                        .frame(width: 120, height: 70)
+                    
+                } placeholder: {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .frame(width: 120, height: 70)
+                }
+                Spacer()
+                Text(match.matchType)
+                Spacer()
+                AsyncImage(url: match.teams[1].flagURL) { image in
+                    image
+                        .resizable()
+                        .frame(width: 120, height: 70)
+                    
+                } placeholder: {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .frame(width: 120, height: 70)
+                }
+            }
+            Text(match.status)
+        }
+    }
 }
+
+//#Preview {
+//    MatchCardView(match: Match(id: "1",
+//                               name: "Dummy Match",
+//                               teams: [.oman, .usa]))
+//}
